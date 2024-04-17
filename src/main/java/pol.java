23e9bc390cc6596cd69ff7fa1d911ee06e67bb30
BartoszS731 <161606110+BartoszS731@users.noreply.github.com>
2024-04-17 12:43:39 +0000

    1

---
+++ src/main/java/pol.java
@@ -0,0 +1,15 @@
+public class pol 
+  {
+    public static void main(String[] arqs){
+      int size =6;
+      for(int i=0;i<size;i++)
+     {
+      for(int j=0;j<=i;j++)
+        {
+          System.out.print("*");
+        }
+       System.out.println();
+      }
+
+  }
+  }
